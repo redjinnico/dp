ad31eaac20d91de2fea43bd8cb3c788600563fff
Chaitanyarana02 <chaitanyaranaa02@gmail.com>
2025-04-04 01:14:34 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/test_portfolio/',
+  // base: '/dp/',
+  
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: [
